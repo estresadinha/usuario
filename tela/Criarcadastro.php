36f7b0767acf24c564524a4b6c2fd24f73175e02
manu <120022356+estresadinha@users.noreply.github.com>
2024-01-19 11:44:54 -0300

    Add files via upload

---
+++ tela/Criarcadastro.php
@@ -0,0 +1,8 @@
+<?php
+echo '<h2>Criarcadastro.php</h2>';
+
+$nome= $_POST['nome'];
+$email= $_POST['email'];
+$usuario = $_POST['usuario'];
+$senha = $_POST ['senha'];
+
---
+++ tela/tela.php
@@ -0,0 +1,32 @@
+<div class="container login-usuario">
+        <h2 class="text-center mb-4">Login</h2>
+ 
+        <form method="post" action="Criarcadastro.php">
+ 
+        <div class="form-group">
+                <label for="usuario">Nome</label>
+                <input type="text" class="forr-control" id="nome" name="nome" placeholder="Digite seu Nome">
+            </div>
+ 
+            <div class="form-group">
+                <label for="usuario">Email</label>
+                <input type="text" class="forr-control" id="email" name="email" placeholder="Digite seu Email">
+            </div>
+ 
+            <div class="form-group">
+                <label for="usuario">Usuario</label>
+                <input type="text" class="forr-control" id="usuario" name="usuario" placeholder="Digite seu Usuário">
+            </div>
+ 
+            <div class="mt-1 form-group">
+                <label for="senha">Senha:</label>
+                <input type="text" class="form-control" id="senha" name="senha" placeholder="Digite a senha">
+            </div>
+           
+ 
+            <div class="mt-2 text-center">
+                <button type="submit" class="btn btn-warning">Login</button>
+            </div>
+ 
+        </form>
+    </div>
